c982aa3607e4f701753ef6ca2b9ff02f89d63f94
HossamGad <54635291+HossamGad@users.noreply.github.com>
2019-09-17 15:40:26 +0200

    Add files via upload

---
+++ övning1/script.js
@@ -0,0 +1,6 @@
+
+ var name = "Hossam Gad";
+document.getElementById("p").innerHTML= name;
+    
+  var saker = [ "Googla" , "Passa tider" , "köra" , "Spela"]
+document.getElementById("div").innerHTML= saker;
